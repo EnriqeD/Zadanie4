d309d878db021fabfa9b40c7b4bd72c5c370720a
EnriqeD <henrykszambelan.pv@gmail.com>
2025-12-12 17:29:28 +0100

    Konstruktor domyślny + alokuj

--- main.cpp
+++ main.cpp
@@ -62,3 +62,7 @@ int main() {
     }
     plik_we.close();
 
+    // 1. Konstruktor domyślny + alokuj
+    matrix m1;
+    m1.alokuj(N);
+    wypisz_fragment(m1, "1. Konstruktor domyslny + alokuj(N) [powinny byc 0]");
